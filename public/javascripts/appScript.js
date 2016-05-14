d9397d3afe6d147b411e31933259d5c89c9fe70f
raviiii1 <ravi.prakash0303@gmail.com>
2016-05-14 19:44:32 +0530

    Error message optimization.
    The watcher for error message removed. Its being handled now using call
    back functions.

--- public/javascripts/appScript.js
+++ public/javascripts/appScript.js
@@ -11,6 +11,11 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
         templateUrl: './register.html',
         controller: 'AuthController'
     });
+    $stateProvider.state('main',{
+        url: '/main',
+        templateUrl: './main.html',
+        controller: 'AuthController'
+    });
     $urlRouterProvider.otherwise('login');
 }]);
 
@@ -21,39 +26,56 @@ app.factory('authService', ['$http', '$window', function ($http, $window) {
         $window.localStorage['login-token'] = token;
         console.log("login_token "+ $window.localStorage['login-token']);
     };
+    authService.getToken = function () {
+        return $window.localStorage['login-token'];
+    }
     authService.login = function (credentials) {
-        $http.post('/login', credentials).success(function (data) {
+        return $http.post('/login', credentials).success(function (data) {
             authService.setToken(data.token);
-        }).error(function (error) {
-            authService.error = error;
         });
     };
     authService.register = function (credentials) {
-        $http.post('/register', credentials).success(function (data) {
+        return $http.post('/register', credentials).success(function (data) {
             console.log(data);
             authService.setToken(data.token);
-        }).error(function (error) {
-            authService.error = error;
         });
     };
     authService.logout = function () {
         $window.localStorage.removeItem('login-token');
         console.log("login_token "+ $window.localStorage['login-token']);
     };
+    authService.isLoggedIn = function () {
+        var token = authService.getToken();
+        if(token){
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.exp > Date.now()/1000;
+        }
+        else {
+            return false
+        }
+    }
     return authService;
 }]);
 
-app.controller('AuthController', ['$scope', 'authService', function ($scope, authService) {
+app.controller('AuthController', ['$scope', '$state', 'authService', function ($scope, $state, authService) {
     $scope.message = "";
     $scope.courseList = ['B.Tech','M.Tech', 'M.C.A.', 'B.C.A.', 'Ph.D'];
     $scope.branchList = ['CSE', 'EE', 'ECE', 'IT', 'CHE', 'CE', 'ME', 'BT','PROD'];
     $scope.batchList = [2012,2013,2014,2015,2016];
     $scope.login = function () {
-        authService.login($scope.log);
+        authService.login($scope.log).error(function (error) {
+            $scope.message = error.message;
+        }).then(function () {
+            $state.go('main');
+        });
         $scope.log = {};
     };
     $scope.register = function () {
-        authService.register($scope.reg);
+        authService.register($scope.reg).error(function (error) {
+            $scope.message = error.message;
+        }).then(function () {
+            $state.go('main');
+        });
         $scope.reg = {};
     };
     $scope.logout = function () {
@@ -62,12 +84,5 @@ app.controller('AuthController', ['$scope', 'authService', function ($scope, aut
     $scope.closeError = function () {
         $scope.message = "";
         authService.error.message = "";
-    }
-    $scope.$watch(function () {
-        return authService.error
-    }, function (newVal, oldVal) {
-        if (newVal !== 'undefined') {
-            $scope.message = authService.error.message;
-        }
-    });
+    };
 }]);
